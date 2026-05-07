1fd1697d1735325c7e58f431b37db8845f3486f0
Nicholas <nicholasmuniz19@gmail.com>
2026-05-07 18:29:01 -0300

    atualizando controller

--- ios/App/App/ViewController.swift
+++ ios/App/App/ViewController.swift
@@ -2,9 +2,4 @@ import UIKit
 import Capacitor
 
 class ViewController: CAPBridgeViewController {
-    override open func capacitorDidLoad() {
-        bridge?.registerPluginInstance(GpsTrackingPlugin())
-        bridge?.registerPluginInstance(LinkaBiometricPlugin())
-        bridge?.registerPluginInstance(WorkoutActivityPlugin())
-    }
 }
