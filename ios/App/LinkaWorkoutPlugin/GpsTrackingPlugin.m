c53eb9b4e0eaf72bdbd1c4dd39a0a2201bd47494
Nicholas <nicholasmuniz19@gmail.com>
2026-08-14 17:59:00 -0300

    correção de bugs e melhorias no modo de treino

--- ios/App/LinkaWorkoutPlugin/GpsTrackingPlugin.m
+++
@@ -1,8 +0,0 @@
-#import <Capacitor/Capacitor.h>
-
-CAP_PLUGIN(GpsTrackingPlugin, "GpsTrackingPlugin",
-    CAP_PLUGIN_METHOD(start, CAPPluginReturnPromise);
-    CAP_PLUGIN_METHOD(stop,  CAPPluginReturnPromise);
-    CAP_PLUGIN_METHOD(addListener, CAPPluginReturnPromise);
-    CAP_PLUGIN_METHOD(removeAllListeners, CAPPluginReturnPromise);
-)
